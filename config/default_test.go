11130dc44cc8c3949ca81c57614ad56b6d9cf709
René Jochum <rene@jochum.dev>
2022-10-02 22:45:14 +0200

    (feat): Remove config/source/(env|flag)

--- config/default_test.go
+++ config/default_test.go
@@ -10,25 +10,9 @@ import (
 	"time"
 
 	"github.com/go-micro/microwire/v5/config/source"
-	"github.com/go-micro/microwire/v5/config/source/env"
 	"github.com/go-micro/microwire/v5/config/source/file"
 	"github.com/go-micro/microwire/v5/config/source/memory"
 )
-
-func createFileForIssue18(t *testing.T, content string) *os.File {
-	data := []byte(content)
-	path := filepath.Join(os.TempDir(), fmt.Sprintf("file.%d", time.Now().UnixNano()))
-	fh, err := os.Create(path)
-	if err != nil {
-		t.Error(err)
-	}
-	_, err = fh.Write(data)
-	if err != nil {
-		t.Error(err)
-	}
-
-	return fh
-}
 
 func createFileForTest(t *testing.T) *os.File {
 	data := []byte(`{"foo": "bar"}`)
@@ -90,44 +74,6 @@ func TestConfigLoadWithInvalidFile(t *testing.T) {
 	}
 	if !strings.Contains(fmt.Sprintf("%v", err), "/i/do/not/exists.json") {
 		t.Fatalf("Expected error to contain the unexisting file but got %v", err)
-	}
-}
-
-func TestConfigMerge(t *testing.T) {
-	fh := createFileForIssue18(t, `{
-  "amqp": {
-    "host": "rabbit.platform",
-    "port": 80
-  },
-  "handler": {
-    "exchange": "springCloudBus"
-  }
-}`)
-	path := fh.Name()
-	defer func() {
-		fh.Close()
-		os.Remove(path)
-	}()
-	os.Setenv("AMQP_HOST", "rabbit.testing.com")
-
-	conf, err := NewConfig()
-	if err != nil {
-		t.Fatalf("Expected no error but got %v", err)
-	}
-	if err := conf.Load(
-		file.NewSource(
-			file.WithPath(path),
-		),
-		env.NewSource(),
-	); err != nil {
-		t.Fatalf("Expected no error but got %v", err)
-	}
-
-	actualHost := conf.Get("amqp", "host").String("backup")
-	if actualHost != "rabbit.testing.com" {
-		t.Fatalf("Expected %v but got %v",
-			"rabbit.testing.com",
-			actualHost)
 	}
 }
 
